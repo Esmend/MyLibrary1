5e19ffeb341ca6a84e9a29b9a4270c3db3a0bd7f
FreeS <kombad056@gmail.com>
2023-12-05 14:42:44 +0500

    ghjghj

---
+++ MyLibrary/MyLib.cs
@@ -0,0 +1,39 @@
+﻿namespace MyLibrary
+{
+    public class MyLib
+    {
+        public void Add(double x, double y)
+        {
+            Console.WriteLine(x + y);
+        }
+        public void Subtract (double x, double y)
+        {
+            Console.WriteLine(x - y);
+        }
+        public void Multiply (double x, double y)
+        {
+            Console.WriteLine(x * y);
+        }
+        public void Divide (double x, double y)
+        { 
+            Console.WriteLine(x / y);
+        }
+
+        public void Pow(double x, double y)
+        {
+            double result = 1;
+            for (double i = 0; i < y; i++)
+            {
+                result *= x;
+            }
+            Console.WriteLine(result);
+        }
+
+        public void Sqrt(double x)
+        {
+            double res = 1;
+            res /= x;
+            Console.WriteLine(res);
+        }
+    }
+}
---
+++ PZ_12/Program.cs
@@ -0,0 +1,14 @@
+﻿using MyLibrary;
+
+namespace PZ_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyLib myLib = new MyLib();
+            myLib.Pow(4,4);
+            
+        }
+    }
+}
